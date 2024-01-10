0c14c0c9f8046cbba82d206861d52e492d5272b4
BOTGITBASH <kk0298156@gmail.com>
2024-01-10 22:19:54 +0700

    Agak lain

--- index.js
+++ index.js
@@ -1,20 +1,89 @@
+const TiktokDownloader = require("./lib/Tiktok-Downloader.js");
+const InstagramDownloader = require("./lib/Instagram-Downloader.js");
+const CapcutDownloader = require("./lib/Capcut-Downloader.js");
+const DriveDownloader = require("./lib/Drive-Downloader.js");
+const MediaFireDownloader = require("./lib/MediaFire-Downloader.js");
+const FacebookDownloader = require("./lib/Facebook-Downloader.js");
+const TwitterDownloader = require("./lib/Twitter-Downloader.js");
+const SpotifyDownloader = require("./lib/Spotify-Downloader.js");
+const PinterestDownloader = require("./lib/Pinterest-Downloader.js");
+const YouTubeDownloader = require("./lib/YouTube-Downloader/Down.js");
+const YouTubeScraper = require("./lib/YouTube-Downloader/Scraper.js");
+
+function downloadTiktokVideo(url) {
+  return new Promise((resolve, reject) => {
+    TiktokDownloader.tikVideo(url)
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function downloadTiktokUser(url) {
+  return new Promise((resolve, reject) => {
+    TiktokDownloader.tikUser(url)
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function downloadYouTubeSearch(...q) {
+  return new Promise((resolve, reject) => {
+    new YouTubeScraper().search(...q)
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function downloadYouTube(...args) {
+  return new Promise((resolve, reject) => {
+    YouTubeDownloader(...args)
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function downloadByService(url) {
+  return new Promise((resolve, reject) => {
+    if (url.match(/tiktok/i)) {
+      resolve(downloadTiktokVideo(url));
+    } else if (url.match(/instagram/i)) {
+      resolve(InstagramDownloader(url));
+    } else if (url.match(/capcut/i)) {
+      resolve(CapcutDownloader(url));
+    } else if (url.match(/drive\.google\.com/i)) {
+      resolve(DriveDownloader(url));
+    } else if (url.match(/mediafire/i)) {
+      resolve(MediaFireDownloader(url));
+    } else if (url.match(/facebook/i)) {
+      resolve(FacebookDownloader(url));
+    } else if (url.match(/twitter/i)) {
+      resolve(TwitterDownloader(url));
+    } else if (url.match(/spotify/i)) {
+      resolve(SpotifyDownloader(url));
+    } else if (url.match(/pinterest/i)) {
+      resolve(PinterestDownloader(url));
+    } else if (url.match(/youtube\.com/i)) {
+      resolve(YouTubeDownloader(url));
+    } else {
+      reject('Unsupported service or invalid URL');
+    }
+  });
+}
+
 module.exports = {
-  TiktokVideo: require("./lib/Tiktok-Downloader.js").tikVideo,
-  TiktokUser: require("./lib/Tiktok-Downloader.js").tikUser,
-  Instagram: require("./lib/Instagram-Downloader.js"),
-  Capcut: require("./lib/Capcut-Downloader.js"),
-  Drive: require("./lib/Drive-Downloader.js"),
-  MediaFire: require("./lib/MediaFire-Downloader.js"),
-  Facebook: require("./lib/Facebook-Downloader.js"),
-  Twitter: require("./lib/Twitter-Downloader.js"),
-  Spotify: require("./lib/Spotify-Downloader.js"),
-  Pinterest: require("./lib/Pinterest-Downloader.js"),
+  TiktokVideo: downloadTiktokVideo,
+  TiktokUser: downloadTiktokUser,
+  Instagram: InstagramDownloader,
+  Capcut: CapcutDownloader,
+  Drive: DriveDownloader,
+  MediaFire: MediaFireDownloader,
+  Facebook: FacebookDownloader,
+  Twitter: TwitterDownloader,
+  Spotify: SpotifyDownloader,
+  Pinterest: PinterestDownloader,
   YouTube: {
-    search: (...q) => {
-      return new (require("./lib/YouTube-Downloader/Scraper.js"))().search(
-        ...q,
-      );
-    },
-    ...require("./lib/YouTube-Downloader/Down.js"),
+    search: downloadYouTubeSearch,
+    down: downloadYouTube,
   },
-};
+  AutoDL: downloadByService
+}
